c5f1ad06913af63d3bcbcc6280e57d05aacaae62
momo631 <631553794@qq.com>
2025-05-23 12:52:20 +0800

    1

--- src/main/java/com/momosensei/all_things_tinkers/modifiers/modifiers/PotionExtension.java
+++ src/main/java/com/momosensei/all_things_tinkers/modifiers/modifiers/PotionExtension.java
@@ -2,14 +2,19 @@ package com.momosensei.all_things_tinkers.modifiers.modifiers;
 
 import com.momosensei.all_things_tinkers.modifiers.ATTmodifier;
 import com.momosensei.all_things_tinkers.register.ATTModifiers;
+import net.minecraft.util.Mth;
 import net.minecraft.world.effect.MobEffect;
 import net.minecraft.world.effect.MobEffectCategory;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.level.Level;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.entity.living.MobEffectEvent;
 import slimeknights.tconstruct.library.modifiers.ModifierEntry;
+import slimeknights.tconstruct.library.tools.helper.ModifierUtil;
 import slimeknights.tconstruct.library.tools.nbt.IToolStackView;
 
 import java.util.ArrayList;
@@ -18,6 +23,7 @@ import java.util.List;
 
 public class PotionExtension extends ATTmodifier {
     public PotionExtension() {
+        //MinecraftForge.EVENT_BUS.addListener(this::AddMobEffect);
     }
     private static final List<MobEffect> BENEFICIAL_EFFECTS = new ArrayList<>(List.of(
             MobEffects.LUCK, MobEffects.DIG_SPEED, MobEffects.FIRE_RESISTANCE, MobEffects.MOVEMENT_SPEED,
@@ -37,10 +43,20 @@ public class PotionExtension extends ATTmodifier {
             MobEffectInstance effect = aeffect.stream().toList().get(i);
             MobEffect mobEffect = effect.getEffect();
             for (MobEffect beneficialeffect : getBeneficialEffectsByCopy()) {
-                if (a > 0 && mobEffect!=beneficialeffect && effect.getDuration() < 5 && effect.getAmplifier() >= 1) {
+                if (a > 0 && mobEffect==beneficialeffect&&!mobEffect.isInstantenous() && effect.getDuration() < 5 && effect.getAmplifier() >= 1) {
                     entity.addEffect(new MobEffectInstance(mobEffect, 200 + 100 * a, effect.getAmplifier() - 1));
                 }
             }
         }
-    }
+    }/*
+    private void AddMobEffect(MobEffectEvent.Applicable event) {
+        if (event.getEntity() instanceof Player player) {
+            int a = getAllModifierlevel(player, ATTModifiers.potionextension.getId());
+            if (a>0){
+                event.getEffectInstance().mapDuration(b->{var effect=event.getEffectInstance().getEffect();
+                if (event.getEffectInstance().isInfiniteDuration()||effect.isInstantenous()||effect.isBeneficial())return b;
+                return (int) (b*(1f+0.1f*a));});
+            }
+        }
+    }*/
 }
